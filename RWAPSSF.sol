88dccdee3647d52413d9a4ff529216dae766360f
Jeammm <87964996+Jeammm@users.noreply.github.com>
2024-02-12 21:04:47 +0700

    using commit-reveal to hide players choice

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+import "./CommitReveal.sol";
 
-contract RWAPSSF {
+contract RWAPSSF is CommitReveal{
     struct Player {
         uint choice;
         // 0 - Rock, 1 - Water , 2 - Air, 3 - Paper, 4 - Sponge, 5 - Sissors, 6 - Fire, 7 - undefined
         address addr;
         uint joinTime;
         uint playTime;
+        uint revealTime;
     }
 
     uint waitingTime = 10;
@@ -17,6 +19,7 @@ contract RWAPSSF {
     uint public reward = 0;
     mapping (uint => Player) public player;
     uint public numInput = 0;
+    uint public numReveal = 0;
 
     function addPlayer() public payable {
         require(numPlayer < 2);
@@ -26,17 +29,35 @@ contract RWAPSSF {
         player[numPlayer].choice = 7;
         player[numPlayer].joinTime = block.timestamp;
         player[numPlayer].playTime = 0;
+        player[numPlayer].revealTime = 0;
         numPlayer++;
     }
 
-    function input(uint choice, uint idx) public  {
+    function input(uint choice, uint idx, string memory salt) public  {
+        //hash input with salt and then commit
         require(numPlayer == 2);
         require(msg.sender == player[idx].addr);
         require(choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6 || choice == 7);
-        player[idx].choice = choice;
+        
+        bytes32 saltHash = keccak256(abi.encodePacked(salt));
+        commit(getSaltedHash(bytes32(choice), saltHash));
+        
         player[idx].playTime = block.timestamp;
         numInput++;
-        if (numInput == 2) {
+    }
+
+    function revealMyChoice(uint choice, uint idx, string memory salt) public {
+        //reveal the answer with the given salt
+        require(numInput == 2);
+        require(msg.sender == player[idx].addr);
+
+        bytes32 saltHash = keccak256(abi.encodePacked(salt));
+        revealAnswer(bytes32(choice), saltHash);
+        player[idx].choice = choice;
+        player[idx].revealTime = block.timestamp;
+        numReveal++;
+        
+        if (numReveal == 2) {
             _checkWinnerAndPay();
         }
     }
@@ -50,7 +71,7 @@ contract RWAPSSF {
             // to pay player[1]
             account1.transfer(reward);
         }
-        else if ((p1Choice + 1) % 3 == p0Choice || (p1Choice + 2) % 3 == p0Choice || (p1Choice + 3) % 3 == p0Choice) {
+        else if ((p1Choice + 1) % 7 == p0Choice || (p1Choice + 2) % 7 == p0Choice || (p1Choice + 3) % 7 == p0Choice) {
             // to pay player[0]
             account0.transfer(reward);    
         }
@@ -66,6 +87,7 @@ contract RWAPSSF {
         require(msg.sender == player[idx].addr);
 
         if (numPlayer == 1) {
+            //no one join the game in time
             require(block.timestamp - player[idx].joinTime > waitingTime);
             address payable account = payable(player[idx].addr);
             account.transfer(reward);
@@ -80,19 +102,32 @@ contract RWAPSSF {
         }
 
         if (numPlayer == 2) {
-            require(player[idx].playTime != 0);
-            require(block.timestamp - player[idx].playTime > waitingTime);
-            require(player[opponent].playTime == 0);
-            address payable account = payable(player[idx].addr);
-            account.transfer(reward);
-            reset();
+            if (numInput == 2) {
+                //opponent not reveal his answer in time
+                require(player[idx].revealTime > waitingTime);
+                require(player[opponent].choice == 7);
+                address payable account = payable(player[idx].addr);
+                account.transfer(reward);
+                reset();
+
+            } else {
+                //opponent not pick a choice in time
+                require(player[idx].playTime != 0);
+                require(block.timestamp - player[idx].playTime > waitingTime);
+                require(player[opponent].playTime == 0);
+                address payable account = payable(player[idx].addr);
+                account.transfer(reward);
+                reset();
+            }
         }
     }
 
     function reset() private {
+        //clean all the states
         numPlayer = 0;
         reward = 0;
         numInput = 0;
+        numReveal = 0;
         delete player[0];
         delete player[1];
     }
